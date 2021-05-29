9acdfa30eb80413f3042fb0c336f399199244d14
Soham Shinde <sohamshinde04@gmail.com>
2021-05-29 12:20:10 +0530

    Added the main activity layout, floating button, menu list There was an error in the toolbar which was removed after adding certain lines

--- app/src/main/java/com/soham/todo/MainActivity.kt
+++ app/src/main/java/com/soham/todo/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.soham.todo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        supportActionBar?.hide();
+
+
         setContentView(R.layout.activity_main)
+
+
+
+        setSupportActionBar(findViewById(R.id.materialToolbar))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_layout, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.historyOption -> {
+                startActivity(Intent(this, HistoryActivity::class.java))
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
